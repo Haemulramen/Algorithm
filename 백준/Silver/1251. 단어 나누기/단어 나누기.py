6c82a7856d99d7424ff363d381d9eb709ceb5817
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-07-11 18:29:59 +0900

    [Silver V] Title: 단어 나누기, Time: 96 ms, Memory: 110432 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.py
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -3,13 +3,13 @@ res_words = []
 
 for i in range(1, len(word)-1):
     for j in range(i+1, len(word)):
-        part1 = word[:i]
-        part2 = word[i:j]
-        part3 = word[j:]
+        part1 = word[i-1::-1]
+        part2 = word[j-1:i-1:-1]
+        part3 = word[len(word)-1:j-1:-1]
 
-        part1.reverse()
-        part2.reverse()
-        part3.reverse()
+        # part1.reverse()
+        # part2.reverse()
+        # part3.reverse()
 
         res_words.append(''.join(part1+part2+part3))
 
