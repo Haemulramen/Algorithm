011da014c07b4739b1db9e213893ce3e112af584
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-07-23 00:19:09 +0900

    [Gold V] Title: 적록색약, Time: 196 ms, Memory: 117992 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -11,27 +11,15 @@ for i in range(N):
         if weak_map[i][j] == 'R':
             weak_map[i][j] = 'G'
 
-def dfs(x, y, color):
-    if x < 0 or x >= N or y < 0 or y >= N or data[x][y] == 'O':
+def dfs(x, y, region, color):
+    if x < 0 or x >= N or y < 0 or y >= N or region[x][y] == 'O':
         return False
-    if data[x][y] == color:
-        data[x][y] = 'O'
-        dfs(x + 1, y, color)
-        dfs(x, y + 1, color)
-        dfs(x - 1, y, color)
-        dfs(x, y - 1, color)
-        return True
-    return False
-
-def dfs_color(x, y, color):
-    if x < 0 or x >= N or y < 0 or y >= N or weak_map[x][y] == 'O':
-        return False
-    if weak_map[x][y] == color:
-        weak_map[x][y] = 'O'
-        dfs_color(x + 1, y, color)
-        dfs_color(x, y + 1, color)
-        dfs_color(x - 1, y, color)
-        dfs_color(x, y - 1, color)
+    if region[x][y] == color:
+        region[x][y] = 'O'
+        dfs(x + 1, y, region, color)
+        dfs(x, y + 1, region, color)
+        dfs(x - 1, y, region, color)
+        dfs(x, y - 1, region, color)
         return True
     return False
 
@@ -40,9 +28,9 @@ color_weak = 0
 
 for i in range(N):
     for j in range(N):
-        if dfs(i, j, data[i][j]):
+        if dfs(i, j, data, data[i][j]):
             normal += 1
-        if dfs_color(i, j, weak_map[i][j]):
+        if dfs(i, j, weak_map, weak_map[i][j]):
             color_weak += 1
 
 print(normal, color_weak)
