d1dec0ec5c8bafee905516243d3b5953b5c16d29
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-08-13 16:19:18 +0900

    [Gold V] Title: 용액, Time: 668 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,36 +1,28 @@
-
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-liquids = list(map(int, input().split()))
+N = int(input())
+data = list(map(int, input().split()))
 
-ans = float("INF")
-ans_left = 0
-ans_right = 0
+left, right = 0, 0
 
-for i in range(n-1):
-    current = liquids[i]
-
-    start = i + 1
-    end = n - 1
-
+diff = 9999999999
+for i in range(N - 1):
+    dt = data[i]
+    start, end = i + 1, N - 1
     while start <= end:
         mid = (start + end) // 2
-        tmp = current + liquids[mid]
+        temp = dt + data[mid]
 
-        if abs(tmp) < ans:
-            ans = abs(tmp)
-            ans_left = i
-            ans_right = mid
-
-            if tmp == 0:
+        if abs(temp) < diff:
+            diff = abs(temp)
+            left = i
+            right = mid
+            if temp == 0:
                 break
-        
-        if tmp < 0:
+        if temp < 0:
             start = mid + 1
-        
         else:
             end = mid - 1
 
-print(liquids[ans_left], liquids[ans_right])
+print(data[left], data[right])
