8a9ca5d597c910ec6677f1ce2a1824e40d83a463
Haemulramen <128495883+Haemulramen@users.noreply.github.com>
2024-08-15 14:40:18 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,19 +1,28 @@
-kg1 = 5
-kg2 = 3
-total = 0
+# 2839 설탕 배달
 
 N = int(input())
+# div = N // 5
+# rem = N % 5
+# temp = -1
+#
+# while div >= 0:
+#     if rem % 3 == 0:
+#         temp = div + rem // 3
+#         break
+#     else:
+#         div -= 1
+#         rem += 5
+#
+# print(temp)
 
-div = N // kg1
-rem = N % kg1
+weights = [5001] * 5001
+weights[3] = 1
+weights[5] = 1
 
-for i in range(div, -1, -1):
-    if rem % kg2 == 0:
-        total = i + rem // kg2
-        break
-    rem += kg1
+for i in range(6, N + 1):
+    weights[i] = min(weights[i - 3] + 1, weights[i - 5] + 1)
 
-if total == 0:
+if weights[N] >= 5001:
     print(-1)
 else:
-    print(total)
+    print(weights[N])
